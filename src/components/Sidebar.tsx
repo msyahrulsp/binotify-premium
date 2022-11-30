83f2b88fe73f762817256407d04a5d677eab3ec2
msyahrulsp <13520161@std.stei.itb.ac.id>
2022-11-30 11:46:45 +0700

    feat: revamp sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,126 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { Routing } from '../routing';
-import { Box, List, ListItem } from '@chakra-ui/react';
+import { useContext } from 'react';
+import { Link, matchPath } from 'react-router-dom';
+import { Flex, HStack, Image, Text, useToast, VStack } from '@chakra-ui/react';
+import { Access } from '../hooks/useRole';
+import { AppContext } from '../context/AppContext';
+import { AppContextProps } from '../@types/context';
+
+import {
+  MdOutlineDashboard,
+  MdOutlineSubscriptions,
+  MdFormatListBulleted,
+  MdLogin,
+  MdLogout
+} from 'react-icons/md';
+import { useAuth } from '../hooks/useAuth';
 
 const Sidebar = () => {
-  const [path, setPath] = useState('/');
-  const singerID = 1;
+  const { user } = useContext(AppContext) as AppContextProps;
+  const { logout } = useAuth();
+  const role = user?.isAdmin ? Access.ADMIN : Access.SINGER;
+  const toast = useToast();
 
-  const navItem = [
-    { path: '/', title: 'Home' },
-    { path: `/singer/${singerID}/songs`, title: 'Song List' }
-  ];
+  const navItem = {
+    ADMIN: [
+      { path: '/', label: 'Dashboard', icon: <MdOutlineDashboard size={25} /> },
+      {
+        path: '/subscription',
+        label: 'Subscription',
+        icon: <MdOutlineSubscriptions size={25} />
+      }
+    ],
+    SINGER: [
+      { path: '/', label: 'Dashboard', icon: <MdOutlineDashboard size={25} /> },
+      {
+        path: `/songs`,
+        label: 'Song List',
+        icon: <MdFormatListBulleted size={25} />
+      }
+    ]
+  };
+
+  const handleLogout = () => {
+    logout();
+    toast({
+      title: 'Success',
+      description: 'Berhasil logout',
+      status: 'success',
+      position: 'top',
+      isClosable: true
+    });
+  };
 
   return (
-    <List w='full' fontSize='xl' pt={4}>
-      {navItem.map((item) => (
-        <ListItem
-          pl={4}
-          py={2}
-          _hover={{ backgroundColor: 'gray.300', color: 'black' }}
-        >
-          <Link to={item.path}>
-            <Box w='full'>{item.title}</Box>
-          </Link>
-        </ListItem>
-      ))}
-    </List>
+    <Flex
+      px={8}
+      py={5}
+      bg='black'
+      color='white'
+      flexWrap='wrap'
+      position='sticky'
+      top={0}
+      left={0}
+      alignSelf='flex-start'
+      flexDirection='column'
+      h='100vh'
+      w='fit-content'
+    >
+      <Link to='/'>
+        <HStack flexDirection='row'>
+          <Image
+            boxSize='50px'
+            objectFit='cover'
+            src='../../public/spotify.png'
+            alt='logo'
+          />
+          <Text fontSize='xl' fontWeight='bold'>
+            Premium
+          </Text>
+        </HStack>
+      </Link>
+      {user !== null ? (
+        <Flex flexDirection='column'>
+          <VStack mt={8} spacing={5} alignItems='flex-start'>
+            {navItem[role].map((item) => {
+              const location = window.location.pathname;
+              const isMatch = matchPath({ path: item.path }, location);
+
+              return (
+                <Link to={item.path}>
+                  <HStack
+                    _hover={{
+                      opacity: 0.8
+                    }}
+                    borderLeft={isMatch ? '5px solid #1DB954' : 'none'}
+                    pl={isMatch ? 3 : 0}
+                  >
+                    {item.icon}
+                    <Text>{item.label}</Text>
+                  </HStack>
+                </Link>
+              );
+            })}
+          </VStack>
+          <HStack
+            mt={16}
+            alignItems='center'
+            _hover={{ opacity: 0.8 }}
+            onClick={handleLogout}
+            cursor='pointer'
+          >
+            <MdLogout size={25} />
+            <Text>Logout</Text>
+          </HStack>
+        </Flex>
+      ) : (
+        <Link to='/login'>
+          <HStack mt={8} alignItems='flex-start' _hover={{ opacity: 0.8 }}>
+            <MdLogin size={25} />
+            <Text>Login</Text>
+          </HStack>
+        </Link>
+      )}
+    </Flex>
   );
 };
 
